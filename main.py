1de60578006780db13a2a9fa19366bd0a75db78b
CeivenLean <guoxiaoli35@gmail.com>
2023-11-15 09:02:37 +0800

    test

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ class MyWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
 
-        self.hello = ["Hallo Welt", "你好，世界", "Hei maailma",
-            "Hola Mundo", "Привет мир"]
+        self.hello = ["Hallo World", "你好，世界"]
 
         self.button = QPushButton("Click me!")
         self.text = QLabel("Hello World")
